86c0e795f15a1c9525e5a9ece76e0de8d008a15e
progheal <lph760210@gmail.com>
2022-11-30 16:17:37 +0800

    Prepare for 2022

---
+++ empty.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "aoc/aoc.h"
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	bool partb = AOC::PartB(argv[0]);
+
+	return 0;
+}
+
