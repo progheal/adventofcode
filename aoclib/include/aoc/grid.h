64d818b6c5460ec79d7b05f8e4b47775c8df7e19
progheal <lph760210@gmail.com>
2022-12-19 10:19:36 +0800

    aoclib: add grid

---
+++ aoclib/include/aoc/grid.h
@@ -0,0 +1,195 @@
+#pragma once
+
+#include <iosfwd>
+
+namespace AOC
+{
+
+// 二維座標結構
+// 由於語意問題，將「座標」及「座標差=向量」兩個概念分開
+
+// 向量，表示座標差
+struct Vector
+{
+    int x, y;
+
+    constexpr Vector(int x = 0, int y = 0) : x(x), y(y) {}
+
+    static const Vector NORTHWEST, NORTH, NORTHEAST,
+                             WEST,             EAST,
+                        SOUTHWEST, SOUTH, SOUTHEAST;
+    static const Vector VonNeumannNeighborhood[4];
+    static const Vector MooreNeighborhood[8];
+
+    constexpr Vector& operator += (const Vector& v) {x += v.x; y += v.y; return *this;}
+    constexpr Vector& operator -= (const Vector& v) {x -= v.x; y -= v.y; return *this;}
+    constexpr Vector& operator *= (int scalar) {x *= scalar; y *= scalar; return *this;}
+    // Vector& operator /= (int scalar) {x /= scalar; y /= scalar; return *this;}
+
+    constexpr Vector CW90() {return Vector{y, -x};}
+    constexpr Vector CCW90() {return Vector{-y, x};}
+    constexpr Vector Rotate180() {return Vector{-x, -y};}
+};
+
+constexpr Vector Vector::NORTHWEST {-1, -1};
+constexpr Vector Vector::NORTH     {-1, 0};
+constexpr Vector Vector::NORTHEAST {-1, 1};
+constexpr Vector Vector::WEST      {0, -1};
+constexpr Vector Vector::EAST      {0, 1};
+constexpr Vector Vector::SOUTHWEST {1, -1};
+constexpr Vector Vector::SOUTH     {1, 0};
+constexpr Vector Vector::SOUTHEAST {1, 1};
+
+constexpr Vector Vector::VonNeumannNeighborhood[4] = {
+    Vector::NORTH,
+    Vector::EAST,
+    Vector::SOUTH,
+    Vector::WEST
+};
+
+constexpr Vector Vector::MooreNeighborhood[8] = {
+    Vector::NORTH,
+    Vector::NORTHEAST,
+    Vector::EAST,
+    Vector::SOUTHEAST,
+    Vector::SOUTH,
+    Vector::SOUTHWEST,
+    Vector::WEST,
+    Vector::NORTHWEST
+};
+
+inline constexpr Vector operator + (Vector v1, const Vector& v2) {return v1 += v2;}
+inline constexpr Vector operator - (Vector v1, const Vector& v2) {return v1 -= v2;}
+inline constexpr Vector operator * (Vector v, int scalar) {return v *= scalar;}
+inline constexpr Vector operator * (int scalar, Vector v) {return v *= scalar;}
+// inline constexpr Vector operator / (Vector v, int scalar) {return v /= scalar;}
+
+std::ostream& operator << (std::ostream& out, const Vector& v)
+{
+    return out << '<' << v.x << ',' << v.y << '>'; 
+}
+
+// 座標
+struct Coord
+{
+    int x, y;
+
+    constexpr Coord(int x = 0, int y = 0) : x(x), y(y) {}
+
+    constexpr Coord& operator += (const Vector& v) {x += v.x; y += v.y; return *this;}
+    constexpr Coord& operator -= (const Vector& v) {x -= v.x; y -= v.y; return *this;}
+    constexpr Vector operator - (const Coord& rhs) const {return {x - rhs.x, y - rhs.y};}
+};
+
+inline constexpr Coord operator + (Coord c, const Vector& v) {return c += v;}
+inline constexpr Coord operator + (const Vector& v, Coord c) {return c += v;} 
+inline constexpr Coord operator - (Coord c, const Vector& v) {return c -= v;}
+
+std::ostream& operator << (std::ostream& out, const Coord& c)
+{
+    return out << '(' << c.x << ',' << c.y << ')'; 
+}
+
+struct CoordOrder
+{
+    constexpr bool operator ()(const Coord& c1, const Coord& c2) const
+    {
+        return c1.x == c2.x ? c1.y < c2.y : c1.x < c2.x;
+    }
+};
+
+template<class TwoDContainer>
+class Grid
+{
+    class RowHelper
+    {
+        Grid* parent;
+        size_t row;
+    public:
+        RowHelper(Grid* p, size_t r) : parent(p), row(r) {}
+        auto& operator[](size_t index)
+        {
+            return parent->at(row, index);
+        }
+        auto& at(size_t index)
+        {
+            return parent->at(row, index);
+        }
+    };
+    class ConstRowHelper
+    {
+        const Grid* parent;
+        size_t row;
+    public:
+        ConstRowHelper(const Grid* p, size_t r) : parent(p), row(r) {}
+        const auto& operator[](size_t index)
+        {
+            return parent->at(row, index);
+        }
+        const auto& at(size_t index)
+        {
+            return parent->at(row, index);
+        }
+    };
+public:
+    typedef std::remove_reference_t<
+            decltype(std::declval<TwoDContainer>()[0][0])
+        > Element_t;
+
+    Grid(TwoDContainer& source, Element_t def = {})
+        : container(source), oob(def),
+          height(std::size(source)), width(std::size(source[0]))
+        {}
+    void setOOB(Element_t def) const
+    {
+        oob = def;
+    }
+    RowHelper operator[](size_t index)
+    {
+        return RowHelper(this, index);
+    }
+    RowHelper at(size_t index)
+    {
+        return RowHelper(this, index);
+    }
+    ConstRowHelper operator [](size_t index) const
+    {
+        return ConstRowHelper(this, index);
+    }
+    ConstRowHelper at(size_t index) const
+    {
+        return ConstRowHelper(this, index);
+    }
+    Element_t& operator[](const Coord& c)
+    {
+        return at(c.x, c.y);
+    }
+    const Element_t& operator[](const Coord& c) const
+    {
+        return at(c.x, c.y);
+    }
+    Element_t& at(size_t x, size_t y)
+    {
+        if(x >= height || y >= width)
+        {
+            external_oob = oob;
+            return external_oob;
+        }
+        else
+            return container[x][y];
+    }
+    const Element_t& at(size_t x, size_t y) const
+    {
+        if(x >= height || y >= width)
+            return oob;
+        else
+            return container[x][y];
+    }
+private:
+    TwoDContainer& container;
+    mutable Element_t oob;
+    mutable Element_t external_oob;
+    size_t width, height;
+};
+
+}
---
+++ aoclib/test/grid.cpp
@@ -0,0 +1,160 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <iostream>
+#include <vector>
+#include "aoc/grid.h"
+using namespace std;
+using namespace testing;
+
+MATCHER_P2(CoordMatches, x, y,
+    (negation ? "!="s : "="s) + "Coord {" + to_string(x) + "," + to_string(y) + "}")
+{
+    return A<AOC::Coord>().MatchAndExplain(arg, result_listener)
+        && (arg.x == x && arg.y == y);
+}
+
+MATCHER_P2(VectorMatches, x, y,
+    (negation ? "!="s : "="s) + "Vector {" + to_string(x) + "," + to_string(y) + "}")
+{
+    return A<AOC::Vector>().MatchAndExplain(arg, result_listener)
+        && (arg.x == x && arg.y == y);
+}
+
+TEST(GridTest, CoordArithmetic)
+{
+    AOC::Coord c {3, 5};
+    AOC::Vector v {2, 7}, w {8, 4};
+
+    EXPECT_THAT(c + v, CoordMatches(5, 12));
+    EXPECT_THAT(c - v, CoordMatches(1, -2));
+    EXPECT_THAT(v + w, VectorMatches(10, 11));
+    EXPECT_THAT(v - w, VectorMatches(-6, 3));
+    EXPECT_THAT(v * 2, VectorMatches(4, 14));
+    EXPECT_THAT(3 * w, VectorMatches(24, 12));
+
+    EXPECT_THAT(c + AOC::Vector::NORTHWEST, CoordMatches(2, 4));
+    EXPECT_THAT(c + AOC::Vector::NORTH,     CoordMatches(2, 5));
+    EXPECT_THAT(c + AOC::Vector::NORTHEAST, CoordMatches(2, 6));
+    EXPECT_THAT(c + AOC::Vector::WEST,      CoordMatches(3, 4));
+    EXPECT_THAT(c + AOC::Vector::EAST,      CoordMatches(3, 6));
+    EXPECT_THAT(c + AOC::Vector::SOUTHWEST, CoordMatches(4, 4));
+    EXPECT_THAT(c + AOC::Vector::SOUTH,     CoordMatches(4, 5));
+    EXPECT_THAT(c + AOC::Vector::SOUTHEAST, CoordMatches(4, 6));
+
+    vector<AOC::Coord> vc;
+    for(auto d : AOC::Vector::VonNeumannNeighborhood)
+    {
+        vc.push_back(c + d);
+    }
+    EXPECT_THAT(vc, UnorderedElementsAre(
+        CoordMatches(2, 5),
+        CoordMatches(3, 4),
+        CoordMatches(3, 6),
+        CoordMatches(4, 5)
+    ));
+
+    vc.clear();
+    for(auto d : AOC::Vector::MooreNeighborhood)
+    {
+        vc.push_back(c + d);
+    }
+    EXPECT_THAT(vc, UnorderedElementsAre(
+        CoordMatches(2, 4),
+        CoordMatches(2, 5),
+        CoordMatches(2, 6),
+        CoordMatches(3, 4),
+        CoordMatches(3, 6),
+        CoordMatches(4, 4),
+        CoordMatches(4, 5),
+        CoordMatches(4, 6)
+    ));
+}
+
+TEST(GridTest, GridRead)
+{
+    vector<string> maze {
+        "ABCDEFGHI",
+        "JKLMNOPQR",
+        "STUVWXYZ@",
+        "abcdefghi",
+        "jklmnopqr",
+        "stuvwxyz$",
+        "123456789"
+    };
+
+    AOC::Grid g(maze, '#');
+    AOC::Coord c {3, 4};
+
+    EXPECT_EQ(g[0][0], 'A');
+    EXPECT_EQ(g[2][3], 'V');
+    EXPECT_EQ(g[c], 'e');
+    EXPECT_EQ(g[6][8], '9');
+    EXPECT_EQ(g[-4][3], '#');
+    EXPECT_EQ(g[8][3], '#');
+    EXPECT_EQ(g[3][-5], '#');
+    EXPECT_EQ(g[5][12], '#');
+    EXPECT_EQ(g[-6][-4], '#');
+    EXPECT_EQ(g[-2][11], '#');
+    EXPECT_EQ(g[9][-3], '#');
+    EXPECT_EQ(g[13][14], '#');
+
+    vector<vector<int>> maze2;
+    int value = 0;
+    for(int i = 0; i < 20; i++)
+    {
+        maze2.push_back({});
+        generate_n(back_inserter(maze2.back()), 30,
+            [&](){return ++value;});
+    }
+    AOC::Grid g2 (maze2);
+    EXPECT_EQ(g2[0][0], 1);
+    EXPECT_EQ(g2[0][9], 10);
+    EXPECT_EQ(g2[1][0], 31);
+    EXPECT_EQ(g2[5][5], 156);
+    EXPECT_EQ(g2[19][29], 600);
+    EXPECT_EQ(g2[-4][10], 0);
+    EXPECT_EQ(g2[25][20], 0);
+    EXPECT_EQ(g2[8][-7], 0);
+    EXPECT_EQ(g2[13][44], 0);
+    EXPECT_EQ(g2[-5][-8], 0);
+    EXPECT_EQ(g2[-2][37], 0);
+    EXPECT_EQ(g2[22][-1], 0);
+    EXPECT_EQ(g2[30][49], 0);
+
+    double m3[5][3] = {
+        {0.25, 0.5, 0.75},
+        {0.0625, 0.125, 0.1875},
+        {0.3125, 0.375, 0.4375},
+        {0.5625, 0.625, 0.6875},
+        {0.8125, 0.875, 0.9375}
+    };
+    AOC::Grid g3(m3, -1);
+    EXPECT_EQ(g3[0][0], 0.25);
+    EXPECT_EQ(g3[2][1], 0.375);
+    EXPECT_EQ(g3[4][2], 0.9375);
+    EXPECT_EQ(g3[-1][1], -1);
+    EXPECT_EQ(g3[7][2], -1);
+    EXPECT_EQ(g3[1][-2], -1);
+    EXPECT_EQ(g3[3][6], -1);
+    EXPECT_EQ(g3[-4][-7], -1);
+    EXPECT_EQ(g3[-3][8], -1);
+    EXPECT_EQ(g3[6][-5], -1);
+    EXPECT_EQ(g3[9][10], -1);
+}
+
+TEST(GridTest, GridWrite)
+{
+    vector<string> maze {
+        "ABCDEFGHI",
+        "JKLMNOPQR",
+        "STUVWXYZ@",
+        "abcdefghi",
+        "jklmnopqr",
+        "stuvwxyz$",
+        "123456789"
+    };
+
+    AOC::Grid g(maze, '#');
+    
+    
+}
