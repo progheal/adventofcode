394dc7a6bfa3cffb866a1ed6aa5c9bf751ca9a6d
progheal <lph760210@gmail.com>
2025-12-12 12:54:06 +0800

    2025 Day 10: Factory (part 2)
    Implements the algorithm from the reddit post mentioned in the comments.

--- 2025/10.cpp
+++ 2025/10.cpp
@@ -1,5 +1,7 @@
 #include <algorithm>
 #include <iostream>
+#include <limits>
+#include <map>
 #include <vector>
 #include <string>
 #include "aoc/aoc.h"
@@ -11,7 +13,10 @@ struct Machine
     vector<unsigned> button;
     vector<int> joltage;
     size_t minPress() const;
-    int minJolt() const;
+    size_t minJolt() const;
+private:
+    mutable map<string, size_t> joltCache;
+    size_t minJoltImpl(const vector<int>& jolt) const;
 };
 
 template<>
@@ -62,10 +67,88 @@ size_t Machine::minPress() const
     return minbutton;
 }
 
-int Machine::minJolt() const
+size_t Machine::minJolt() const
 {
-    // TODO
-    return 0;
+#ifdef DEBUG
+    cerr << "=====\n";
+#else
+    cerr << "Running ";
+    for(int j : joltage) cerr << j << "/";
+    cerr << "\n";
+#endif
+    joltCache.clear();
+    string z;
+    for(int i = 0; i < joltage.size(); i++) z += "0/";
+    joltCache[z] = 0;
+    return minJoltImpl(joltage);
+}
+
+size_t Machine::minJoltImpl(const vector<int>& jolt) const
+{
+    // Implements solution from https://redd.it/1pk87hl
+    string key;
+    for(auto j : jolt) key += to_string(j) + "/";
+    if(auto p = joltCache.find(key); p != joltCache.end())
+    {
+#ifdef DEBUG
+        cerr << "Cache hit: " << key << " = " << p->second << "\n";
+#endif
+        return p->second;
+    }
+#ifdef DEBUG
+    cerr << "Begin calculate " << key << "\n";
+#endif
+    unsigned target = 0;
+    for(int i = 0 ; i < jolt.size(); i++) target |= (unsigned)(jolt[i] % 2) << i;
+    size_t btns = button.size();
+    unsigned max = 1 << btns;
+    size_t minbutton = numeric_limits<size_t>::max();
+    for(unsigned m = 0; m < max; m++)
+    {
+        size_t count = 0;
+        vector<int> smalljolt = jolt;
+        for(size_t b = 0; b < btns; b++)
+        {
+            if(m & (1 << b))
+            {
+                ++count;
+                for(int i = 0; i < jolt.size(); i++)
+                {
+                    smalljolt[i] -= button[b] & (1 << i) ? 1 : 0;
+                }
+            }
+        }
+        bool canRecurse = true;
+        for(int i = 0; i < jolt.size(); i++)
+        {
+            if(smalljolt[i] % 2 != 0 || smalljolt[i] < 0)
+            {
+                canRecurse = false;
+                break;
+            }
+            smalljolt[i] /= 2;
+        }
+        if(canRecurse)
+        {
+#ifdef DEBUG
+            cerr << key << " - ";
+            for(int i = 0; i < btns; i++) cerr << (m & (1 << i) ? '1' : '0');
+            cerr << " = ";
+            for(int i = 0; i < jolt.size(); i++) cerr << smalljolt[i] << "/";
+            cerr << "\n";
+#endif
+            size_t res = minJoltImpl(smalljolt);
+            if(res < numeric_limits<size_t>::max())
+            {
+                res = res * 2 + count;
+                minbutton = min(minbutton, res);
+            }
+        }
+    }
+#ifdef DEBUG
+    cerr << "Calculate " << key << " = " << minbutton << "\n";
+#endif
+    return joltCache[key] = minbutton;
 }
 
 int main(int argc, char* argv[])
